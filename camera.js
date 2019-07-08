0b1b4f71016d401efe9f4266631811a6ea09a847
avanish269 <avanish269@gmail.com>
2019-07-08 19:26:41 +0530

    seventh

--- camera.js
+++ camera.js
@@ -2,59 +2,11 @@ const vid=document.getElementById("vid");
 const cv=document.getElementById("cv");
 const ctxt=cv.getContext('2d');
 const capBut=document.getElementById("capture");
-//const select=document.getElementById("select");
 var img=document.createElement("a");
-/*var isMobile={
-  Android: function(){
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBerry: function(){
-    return navigator.userAgent.match(/BlackBerry/i);
-  },
-  iOS: function(){
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  },
-  Opera: function(){
-    return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows: function(){
-    return navigator.userAgent.match(/IEMobile/i);
-  },
-  any: function() {
-    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-  }
-};*/
-
-/*function gotDevices(mediaDevices) {
-  select.innerHTML = '';
-  select.appendChild(document.createElement('option'));
-  let count = 1;
-  mediaDevices.forEach(mediaDevice => {
-    if (mediaDevice.kind === 'videoinput') {
-      const option = document.createElement('option');
-      option.value = mediaDevice.deviceId;
-      const label = mediaDevice.label || `Camera ${count++}`;
-      const textNode = document.createTextNode(label);
-      option.appendChild(textNode);
-      select.appendChild(option);
-    }
-  });
-}*/
-/*const cst={
-  video:true,
-};*/
-//if(isMobile.any()==false){
 cv.style.display="none";
-  const cst={
-    video:{facingMode:"environment"},
-  };
-//}
-/*else {
-  const cst={
-    video: true,
-  };
-}*/
-
+const cst={
+  video:{facingMode:"environment"},
+};
 capBut.addEventListener('click',()=>{
   ctxt.drawImage(vid,0,0,cv.width,cv.height);
   cv.toBlob(function(blob){
@@ -67,5 +19,3 @@ capBut.addEventListener('click',()=>{
 navigator.mediaDevices.getUserMedia(cst).then((stream)=>{
   vid.srcObject=stream;
 });
-
-//navigator.mediaDevices.enumerateDevices().then(gotDevices);
